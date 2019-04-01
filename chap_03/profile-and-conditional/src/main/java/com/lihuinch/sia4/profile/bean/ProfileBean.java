d56951bdaf27add4fd11956fdaa9599a2118f653
lihuinch <lihuinch@163.com>
2019-04-01 21:59:17 +0800

    对第三章的项目结构调整

---
+++ chap_03/profile-and-conditional/src/main/java/com/lihuinch/sia4/profile/bean/ProfileBean.java
@@ -0,0 +1,8 @@
+package com.lihuinch.sia4.profile.bean;
+
+/**
+ * @author lihuinch
+ * @date 2019/3/31 18:54
+ */
+public class ProfileBean {
+}
--- chap_03/src/main/java/com/lihuinch/sia4/profile/bean/ProfileBean.java
+++
@@ -1,8 +0,0 @@
-package com.lihuinch.sia4.profile.bean;
-
-/**
- * @author lihuinch
- * @date 2019/3/31 18:54
- */
-public class ProfileBean {
-}
