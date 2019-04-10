59f2547db43c91dfeb970ec917a392a49ade8b83
lihuinch <lihuinch@163.com>
2019-04-10 08:04:25 +0800

    原来@ComponentScan 加不加@Configration都能运行

--- chap_03/profile-and-conditional/src/main/java/com/lihuinch/sia4/conditional/config/ConditionalAutoConfig.java
+++ chap_03/profile-and-conditional/src/main/java/com/lihuinch/sia4/conditional/config/ConditionalAutoConfig.java
@@ -2,11 +2,14 @@ package com.lihuinch.sia4.conditional.config;
 
 import com.lihuinch.sia4.conditional.bean.ConditionalBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author lihuinch
  * @date 2019/4/1 21:40
  */
+// 实际上，一开始没加上Configuration注解也能使用
+@Configuration
 @ComponentScan(basePackageClasses = ConditionalBean.class)
 public class ConditionalAutoConfig {
 }
--- chap_03/profile-and-conditional/src/main/java/com/lihuinch/sia4/profile/config/ProfileAutoConfig.java
+++ chap_03/profile-and-conditional/src/main/java/com/lihuinch/sia4/profile/config/ProfileAutoConfig.java
@@ -2,6 +2,7 @@ package com.lihuinch.sia4.profile.config;
 
 import com.lihuinch.sia4.profile.bean.ProfileBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 关于profile实验的自动扫描config
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
  * @author lihuinch
  * @date 2019/3/31 18:53
  */
+// 实际上，一开始没加上Configuration注解也能使用
+@Configuration
 @ComponentScan(basePackageClasses = ProfileBean.class)
 public class ProfileAutoConfig {
 
